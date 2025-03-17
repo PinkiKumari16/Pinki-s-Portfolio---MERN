c3b2506f1c869c9a52e8009bc4d8d64370468ccb
PinkiKumari16 <pinkikmr2672002@gmail.com>
2025-03-17 10:46:10 +0530

    combine the backend and frontend with one port 5000.

--- backend/script.js
+++ backend/script.js
@@ -3,21 +3,20 @@ const app = express();
 const dbConfig = require("./config/dbConfig");
 const portfolioRoute = require("./routes/portfolioRoute");
 // const updateInrtoRoute = require("./routes/updateInrtoRoute");
+const path = require("path");
 
 const port = process.env.PORT || 5000;
+const _dirname = path.resolve();
 
-const path = require("path");
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, "frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend/build/index.html"));
-  })
-}
 
 app.use(express.json());
 app.use("/api/portfoliodata", portfolioRoute);
 // app.use("/adminPanel", updateInrtoRoute);
 
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get('*', (_, res)=>{
+  res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+});
 
 
 app.listen(port, () => {
